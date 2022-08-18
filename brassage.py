46dfffcb3d33f6b051639233b8c0cb48b2932ff1
xinxucanada <e2194517@cmaisonneuve.qc.ca>
2022-08-18 15:30:14 -0400

    modify

--- brassage.py
+++ brassage.py
@@ -1,14 +1,7 @@
 
 
-#Évite d'écrire du code d'exécution normale(indentation de gauche) avant tes définitions de fonction
-#Bonne utilisation de tableaux pour générer ton paquet.
-type=['♦','♣','♥','♠']
-carteNb=['A','2','3','4','5','6','7','8','9','10','J','Q','K']
-carteListe=[]
-for i in type:
-    for j in carteNb:
-        carteListe.append(j+i)
-# print(carteListe)
+
+
 def affichage(cartes):
     for i in range(4):
         for j in range(13):
@@ -16,10 +9,10 @@ def affichage(cartes):
         print()
 #Dans ce cas-ci, 26 fonctionne mais idéalement trouve la valeur de manière dynamique (len(cartes)//2)
 def brasseCartes(cartes):
-    paquet1=cartes[:26]
-    paquet2=cartes[26:]
+    paquet1=cartes[:len(cartes)//2]
+    paquet2=cartes[len(cartes)//2:]
     newCarteliste=[]
-    for i in range(26):
+    for i in range(len(cartes)//2):
         newCarteliste.append(paquet1[i])
         newCarteliste.append(paquet2[i])
     cartes=newCarteliste
@@ -28,7 +21,7 @@ def brasseCartes(cartes):
 def sauvegard(cartes):
     f=open("cards.txt","w",encoding="utf-8")
     for i in range(4):
-        for j in range(13):
+        for j in range(len(cartes)//4):
             f.write(cartes[i*13+j]+"\t")
         f.write("\n")
     f.close()
@@ -41,6 +34,16 @@ def opptions():
     option = input("Choisissez: ")
     return option
 
+#Évite d'écrire du code d'exécution normale(indentation de gauche) avant tes définitions de fonction
+#Bonne utilisation de tableaux pour générer ton paquet.
+
+type=['♦','♣','♥','♠']
+carteNb=['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+carteListe=[]
+
+for i in type:
+    for j in carteNb:
+        carteListe.append(j+i)
 ans=opptions()
 
 while ans!="4" and ans!="3":
